13b222f8d44010963677d562d4a88f69957896b3
Jan Beneš <honzikbenes41@seznam.cz>
2025-12-02 19:02:07 +0100

    removed something

--- lib/supabase/proxy.ts
+++ lib/supabase/proxy.ts
@@ -6,13 +6,7 @@ export async function updateSession(request: NextRequest) {
   let supabaseResponse = NextResponse.next({
     request,
   });
-
-  // If the env vars are not set, skip proxy check. You can remove this
-  // once you setup the project.
-  if (!hasEnvVars) {
-    return supabaseResponse;
-  }
-
+  
   // With Fluid compute, don't put this client in a global environment
   // variable. Always create a new one on each request.
   const supabase = createServerClient(
